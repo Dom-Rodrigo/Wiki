2123747127c7eb9f543eaaaeca115224391f4acf
Dom-Rodrigo <rodalpiropo@gmail.com>
2023-04-28 15:56:52 -0300

    Add searching and primitive similar results

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,11 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import util
 
+"""class NewTaskForm(forms.Form):
+    task = forms.CharField()"""
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.GET:
+        query = request.GET["q"]
+        if query in util.list_entries():
+            return render(request, "encyclopedia/get_entry.html", {
+                "entry": util.get_entry(query)
+            })
+        
+        else:
+            similar_entries = []
+            for entry in util.list_entries():
+                if query.upper() in entry.upper():
+                    similar_entries.append(entry)
+
+            return render(request, "encyclopedia/search_results.html", {
+                "query": query,
+                "similar_entries": similar_entries
+            })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 def get_entry(request, query):
     if query in util.list_entries():
