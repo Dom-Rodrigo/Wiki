bbdf81852160efcc8f389d4b7edb74ca076f3d43
Dom-Rodrigo <rodalpiropo@gmail.com>
2023-05-01 15:13:20 -0300

    Random route bug fixed

--- encyclopedia/context_processors.py
+++ encyclopedia/context_processors.py
@@ -2,4 +2,5 @@ import random
 from . import util
 
 def sort_random(request):
-    return {"random": random.choice(util.list_entries())}
+    print(request)
+    return {"random": f"/{random.choice(util.list_entries())}"}
