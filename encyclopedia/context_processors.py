61705c9272ac2ff081ef83b9a48e58e96a3a3deb
Dom-Rodrigo <rodalpiropo@gmail.com>
2023-05-01 16:06:42 -0300

    Conflicting new bug fixed

--- encyclopedia/context_processors.py
+++ encyclopedia/context_processors.py
@@ -2,4 +2,4 @@ import random
 from . import util
 
 def sort_random(request):
-    return {"random": f"/{random.choice(util.list_entries())}"}
+    return {"random": f"{random.choice(util.list_entries())}"}
--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.new_page, name="new"),
-    path("<str:query>/", views.get_entry, name="get_entry"),
-    path("<str:query>/edit/", views.edit, name="edit")
+    path("wiki/<str:query>/", views.get_entry, name="get_entry"),
+    path("wiki/<str:query>/edit/", views.edit, name="edit")
 ]
